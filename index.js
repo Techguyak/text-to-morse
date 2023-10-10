028c5e588e1758df5cf3362e549877b996080e29
Techguyak --git <arun02002047@gmail.com>
2023-10-10 22:41:41 +0530

    js added

--- index.js
+++ index.js
@@ -30,17 +30,17 @@ let match ={
 function a (){
 let input = document.getElementById('input').value;
 input = input.toUpperCase();
-let arr = input.split(" ");
+let arr = input.split("");
 console.log(arr);
+
 let arr1 = arr.map(x =>{
     if (match[x]){
         return match[x];
-
     }
     
     
 })
     let arr2 = arr1.join(" ");
     document.getElementById('output').value = arr2;
-    console.log(arr2);
+    
 }
